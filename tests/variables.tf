c4d60194c401cff921bce7bc1e2c0087bce925db
Utkarsh Sharma <utkarshredhat@gmail.com>
2024-07-15 19:33:13 +0530

    updated the code

--- tests/variables.tf
+++ tests/variables.tf
@@ -1,4 +1,11 @@
 variable "project_id" {
   description = "Google cloud project id to create redis cluster."
   type        = string
+  default     = "pocs-370205"
+}
+
+variable "network_name" {
+  description = "GCP Network Name"
+  type        = string
+  default     = "gcp-vpc"
 }
